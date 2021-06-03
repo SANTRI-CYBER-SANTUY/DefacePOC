9b5718983f310f05843e5387418d94675ba71452
AKKUSANTUY <83626187+SANTRI-CYBER-SANTUY@users.noreply.github.com>
2021-06-03 11:41:33 +0700

    Update DefacePOC.sh

--- DefacePOC.sh
+++ DefacePOC.sh
@@ -12,47 +12,56 @@ clear
 echo
 sleep 1
 echo $green"×××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
-echo $purple" (+) AUTHOR       : SANTRI CYBER SANTUY   (+)"
-echo $purple" (+) INSTAGRAM    : @AKKU_SANTUY          (+)"
-echo $purple" (+) WHATSAPP     : +6282134566596        (+)"
+echo $purple" (+) AUTHOR       : SANTRI CYBER SANTUY      (+)"
+echo $purple" (+) INSTAGRAM    : @AKKU_SANTUY             (+)"
+echo $purple" (+) WHATSAPP     : +6282134566596           (+)"
 echo $green"×××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
+echo $green"============================================================="
 echo $white
-echo "BERIKUT ADALAH WEBSITE YG BISA KAMU DEFACE DGN MUDAH CUK"
-echo $green"======================================================="
+echo "[!] BERIKUT ADALAH WEBSITE YG BISA KAMU DEFACE DGN MUDAH CUK [!]"
+echo $green"============================================================="
 sleep 1
 echo
-echo $white"1.) " $red"REGU.MOE"
-echo $white"2.) " $red"KOSRED.COM"
-echo $white"3.) " $red"NEKO.AIRFORCE"
-echo $white"4.) " $red"DORK ASLINYA"
-echo $white"5.) " $red"DEFACE GWE"
-echo $white"6.) " $red"KELUAR"
+echo $white"1.> " $red"SOBATJASA.COM"
+echo $white"2.> " $red"REGU.MOE"
+echo $white"3.> " $red"KOSRED.COM"
+echo $white"4.> " $red"NEKO.AIRFORCE"
+echo $white"5.> " $red"DORK ASLINYA"
+echo $white"6.> " $red"DEFACE GWE"
+echo $white"7.> " $red"KELUAR"
 echo $green
 sleep 1
-read -p "MASUKAN PILIHAN MU COK => " ger
+read -p ">> PILIH MENU CUK =>> " ger
 
 if [ $ger = 1 ]
 then
 clear
 sleep 1
-xdg-open https://regu.moe/
+xdg-open https://sobatjasa.com/
 fi
 
 if [ $ger = 2 ]
 then
 clear
 sleep 1
-xdg-open https://kosred.com/
+xdg-open https://regu.moe/
 fi
 
 if [ $ger = 3 ]
 then
 clear
 sleep 1
-xdg-open https://img.neko.airforce/
+xdg-open https://kosred.com/
 fi
 
 if [ $ger = 4 ]
+then
+clear
+sleep 1
+xdg-open https://img.neko.airforce/
+fi
+
+if [ $ger = 5 ]
 then
 clear
 echo
@@ -63,13 +72,13 @@ echo
 echo $yellow"("$white" intext: select or drop file(s)"$ywllow" )"
 fi
 
-if [ $ger = 5 ]
+if [ $ger = 6 ]
 then
 clear
 sleep 1
 xdg-open https://santri-cyber-santuy.github.io/
 fi
-if [ $ger = 6 ]
+if [ $ger = 7 ]
 then
    exit -y
 fi
